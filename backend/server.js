b60d976c16cb6055fc000025dccc82081799443f
Chebolu Saibalaji <saibalajichebolu@gmail.com>
2026-02-22 16:24:08 +0530

    Fix cookie and CORS for production-2

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,22 @@ import adminRoutes   from './routes/adminRoutes.js'
 const app = express()
 connectDB()
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://profile-management-five.vercel.app"
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    process.env.CLIENT_URL
-  ],
-  credentials: true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 
